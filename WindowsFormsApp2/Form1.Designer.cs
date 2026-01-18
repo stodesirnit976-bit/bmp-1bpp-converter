b85bdc9f2a6b6e90b0576158ea2fe60a8a10eea7
Panda <stodesirnit976@gmail.com>
2026-01-18 18:30:10 +0800

    添加项目文件。

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tbx_wight = new System.Windows.Forms.TextBox();
+            this.tbx_height = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbx_name = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_work = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 10);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 55);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "选择文件";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btn_ImgImport_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(221, 10);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(328, 257);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tbx_wight
+            // 
+            this.tbx_wight.Location = new System.Drawing.Point(31, 213);
+            this.tbx_wight.Multiline = true;
+            this.tbx_wight.Name = "tbx_wight";
+            this.tbx_wight.Size = new System.Drawing.Size(121, 41);
+            this.tbx_wight.TabIndex = 2;
+            this.tbx_wight.Text = "1234";
+            // 
+            // tbx_height
+            // 
+            this.tbx_height.Location = new System.Drawing.Point(31, 295);
+            this.tbx_height.Multiline = true;
+            this.tbx_height.Name = "tbx_height";
+            this.tbx_height.Size = new System.Drawing.Size(121, 41);
+            this.tbx_height.TabIndex = 2;
+            this.tbx_height.Text = "1234";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 12F);
+            this.label1.Location = new System.Drawing.Point(10, 226);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "W";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 12F);
+            this.label2.Location = new System.Drawing.Point(10, 311);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "H";
+            // 
+            // tbx_name
+            // 
+            this.tbx_name.Location = new System.Drawing.Point(31, 118);
+            this.tbx_name.Multiline = true;
+            this.tbx_name.Name = "tbx_name";
+            this.tbx_name.Size = new System.Drawing.Size(121, 28);
+            this.tbx_name.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 12F);
+            this.label3.Location = new System.Drawing.Point(28, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "保存为文件名";
+            // 
+            // btn_work
+            // 
+            this.btn_work.Location = new System.Drawing.Point(231, 294);
+            this.btn_work.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_work.Name = "btn_work";
+            this.btn_work.Size = new System.Drawing.Size(128, 55);
+            this.btn_work.TabIndex = 0;
+            this.btn_work.Text = "转换";
+            this.btn_work.UseVisualStyleBackColor = true;
+            this.btn_work.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 360);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbx_height);
+            this.Controls.Add(this.tbx_name);
+            this.Controls.Add(this.tbx_wight);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn_work);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox tbx_wight;
+        private System.Windows.Forms.TextBox tbx_height;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbx_name;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_work;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        string inputPath;
+        string outputPath = @"D:\img";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+
+
+
+        private void btn_ImgImport_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dialog = new OpenFileDialog())
+            {
+                dialog.Filter = "IMG Files (*.png;*.bmp;*.jpg)|*.png;*.bmp;*.jpg";
+                dialog.Title = "选择 需要转换的图片文件";
+                dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+                dialog.AutoUpgradeEnabled = false; 
+
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    string IMGPath = dialog.FileName;
+
+                    try
+                    {
+                        inputPath = IMGPath;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+        }        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int targetWidth = int.Parse(tbx_wight.Text);
+            int targetHeight = int.Parse(tbx_height.Text);
+
+            if(pictureBox1.Image != null)
+                pictureBox1.Image.Dispose();
+
+            string dir = Path.GetDirectoryName(outputPath);
+            if (!string.IsNullOrEmpty(dir) && !Directory.Exists(dir))
+            {
+                Directory.CreateDirectory(dir);
+            }
+
+            Image originalImage = null;
+
+            try
+            {
+                byte[] imageBytes = File.ReadAllBytes(inputPath);
+                using (MemoryStream ms = new MemoryStream(imageBytes))
+                {
+                    originalImage = Image.FromStream(ms);
+
+                    using (Bitmap resizedImage = new Bitmap(targetWidth, targetHeight, PixelFormat.Format24bppRgb))
+                    {
+                        resizedImage.SetResolution(originalImage.HorizontalResolution, originalImage.VerticalResolution);
+
+                        using (Graphics g = Graphics.FromImage(resizedImage))
+                        {
+                            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                            g.SmoothingMode = SmoothingMode.HighQuality;
+                            g.PixelOffsetMode = PixelOffsetMode.HighQuality;
+                            g.CompositingQuality = CompositingQuality.HighQuality;
+
+                            g.DrawImage(originalImage, new Rectangle(0, 0, targetWidth, targetHeight),
+                                        new Rectangle(0, 0, originalImage.Width, originalImage.Height),
+                                        GraphicsUnit.Pixel);
+                        }
+
+                        using (MemoryStream outStream = new MemoryStream())
+                        {
+                            Bitmap b = Convert24bppTo1bpp(resizedImage);
+                            
+                            string fullPath = Path.Combine(outputPath, tbx_name.Text.Trim() + ".bmp");
+                   
+                            b.Save(fullPath, ImageFormat.Png);
+                           
+                            pictureBox1.Image = b;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"处理失败: {ex.Message}");
+                throw;
+            }
+            
+        
+        }
+
+
+        public  Bitmap Convert24bppTo1bpp(Bitmap source)
+        {
+            int w = source.Width;
+            int h = source.Height;
+
+            Bitmap bmp = new Bitmap(w, h, PixelFormat.Format1bppIndexed);
+            BitmapData srcData = source.LockBits(new Rectangle(0, 0, w, h), ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+            BitmapData dstData = bmp.LockBits(new Rectangle(0, 0, w, h), ImageLockMode.WriteOnly, PixelFormat.Format1bppIndexed);
+
+            int srcStride = srcData.Stride;
+            byte[] srcBuffer = new byte[srcStride * h];
+            System.Runtime.InteropServices.Marshal.Copy(srcData.Scan0, srcBuffer, 0, srcBuffer.Length);
+
+            int dstStride = dstData.Stride;
+            byte[] dstBuffer = new byte[dstStride * h];
+
+            for (int y = 0; y < h; y++)
+            {
+                int srcRow = y * srcStride;
+                int dstRow = y * dstStride;
+
+                for (int x = 0; x < w; x++)
+                {
+                    int pixelIndex = srcRow + x * 3;
+                    byte b = srcBuffer[pixelIndex];
+                    byte g = srcBuffer[pixelIndex + 1];
+                    byte r = srcBuffer[pixelIndex + 2];
+
+                    if ((r + g + b) / 3 > 128) dstBuffer[dstRow + (x / 8)] |= (byte)(0x80 >> (x % 8));
+                }
+            }
+            System.Runtime.InteropServices.Marshal.Copy(dstBuffer, 0, dstData.Scan0, dstBuffer.Length);
+            bmp.UnlockBits(dstData);
+            source.UnlockBits(srcData);
+
+            return bmp;
+        }
+
+
+
+    }
+}
